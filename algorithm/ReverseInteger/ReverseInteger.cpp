e842b9e31bc9ce022a904c891de5ce6caca0c42c
ahljjun <liu.ljjun@gmail.com>
2017-07-07 22:58:35 +0800

    ReverseInteger

---
+++ algorithm/ReverseInteger/ReverseInteger.cpp
@@ -0,0 +1,37 @@
+
+#include <iostream>
+using namespace std;
+
+const int cIntMax = ~(1<<31);
+
+int reverse(int x)
+{
+    long long y = x; // need to use the long long type , if int y;  y=-y might overflow already
+    if (y<0)
+        y=-y; 
+    else
+        y=y;
+
+    int result=0;
+    while(y){
+        result = 10*result + y%10;
+        y=y/10;
+        if ( y && (result > (cIntMax /10) ||( (result == (cIntMax/10)) && y > 2))) // cIntMax 
+            return 0;
+    }
+
+    if (x<0)
+        return -result;
+    else
+        return result;
+}
+
+int main()
+{
+    cout<<sizeof(long long)<<endl;
+    cout<<cIntMax<<endl;
+
+    cout<<reverse(1534236469)<<endl;
+    cout<<reverse(-2147483412)<<endl;
+    cout<<reverse(-2147483648)<<endl;
+}
