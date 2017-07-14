8c07463f720426dc61eba604c6972b6bdcade862
LiuJun <liu.ljjun@gmail.com>
2017-07-14 18:24:51 +0800

    inital solution

--- algorithm/3Sum/3Sum.cpp
+++ algorithm/3Sum/3Sum.cpp
@@ -12,7 +12,54 @@ A solution set is:
   [-1, -1, 2]
 ]
 */
-
+/*
 vector<vector<int>> threeSum(vector<int>& nums) 
 {
+
+}
+*/
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+vector<vector<int>> threeSum(vector<int>& nums){
+    vector<vector<int>>  ret;
+    sort(nums.begin(), nums.end());
+    int i=0;
+    int j = nums.size()-1;
+    int valToFind = 0;
+    while( i< j ){
+        cout <<"i,j"<<i<<" "<<j<<endl;
+        valToFind = -(nums[i] + nums[j]);
+        if ( valToFind < nums[i+1] ){
+            i++;
+            continue;
+        }
+        if ( valToFind > nums[j-1] ){
+            j--;
+            continue;
+        }
+        //try to find the value in the [i+1, j-1]
+        for(int k=i+1; k <= j-1; k++){
+            if (nums[k] == valToFind){
+                ret.push_back({nums[i], nums[k], nums[j]});
+                i=k ;
+            }
+        }
+    } 
+
+    return ret;
+}   
+
+int main()
+{
+   vector<int> nums = {-1, 0, 1, 2, -1, -4}; 
+   vector<vector<int>> ret = threeSum(nums);
+   for(auto v : ret){
+      cout<<"[";
+      for(auto num : v)
+         cout<<num<<",";
+      cout<<"]";
+   }
 }
