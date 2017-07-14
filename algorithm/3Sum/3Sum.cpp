13e22bcc2b562f3279ee003d435246fa0be06248
ahljjun <liu.ljjun@gmail.com>
2017-07-14 21:05:12 +0800

    update solution

--- algorithm/3Sum/3Sum.cpp
+++ algorithm/3Sum/3Sum.cpp
@@ -19,47 +19,73 @@ vector<vector<int>> threeSum(vector<int>& nums)
 }
 */
 #include <vector>
+#include <set>
 #include <iostream>
 #include <algorithm>
 using namespace std;
 
+
+
+// This version has some issue
 vector<vector<int>> threeSum(vector<int>& nums){
-    vector<vector<int>>  ret;
+    //vector<vector<int>>  ret;
+    set<vector<int>> ret;
     sort(nums.begin(), nums.end());
     int i=0;
     int j = nums.size()-1;
     int valToFind = 0;
-    while( i< j ){
-        cout <<"i,j"<<i<<" "<<j<<endl;
+    while( i < j-1 ){ //at lease 3 values
+        bool found = false;
         valToFind = -(nums[i] + nums[j]);
+        cout <<"i,j,val"<<i<<" "<<j<<","<<valToFind<<endl;
         if ( valToFind < nums[i+1] ){
-            i++;
+            j--;
             continue;
         }
         if ( valToFind > nums[j-1] ){
-            j--;
+            i++; 
             continue;
         }
         //try to find the value in the [i+1, j-1]
         for(int k=i+1; k <= j-1; k++){
             if (nums[k] == valToFind){
-                ret.push_back({nums[i], nums[k], nums[j]});
-                i=k ;
+                cout <<"ValueFound,k: "<<nums[k]<<endl;
+                ret.insert({nums[i], nums[k], nums[j]});
+                i++;
+                j--;
+                found = true;
+                break;
             }
         }
+        if(!found) //how to handle 
+            i++;
     } 
 
-    return ret;
+    return vector<vector<int>> (ret.begin(), ret.end());
 }   
 
-int main()
+
+
+void printVectors(const vector<vector<int>> &vec)
 {
-   vector<int> nums = {-1, 0, 1, 2, -1, -4}; 
-   vector<vector<int>> ret = threeSum(nums);
-   for(auto v : ret){
+   for(auto v : vec){
       cout<<"[";
       for(auto num : v)
          cout<<num<<",";
       cout<<"]";
    }
+}
+
+int main()
+{
+   vector<int> nums1 = {-1, 0, 1, 2, -1, -4}; 
+   vector<int> nums2 = {0, 0,0,0}; 
+   vector<int> nums3 = {1,2,-2,-1}; 
+   vector<int> nums4 = {-1,0,1,0}; 
+   vector<int> nums5 = {-2, 0,1,1,2}; 
+   printVectors(threeSum(nums1));
+   printVectors(threeSum(nums2));
+   printVectors(threeSum(nums3));
+   printVectors(threeSum(nums4));
+   printVectors(threeSum(nums5));
 }
