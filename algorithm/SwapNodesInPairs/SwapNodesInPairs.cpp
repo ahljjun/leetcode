2a727310ed38aa90919d003ac82110798aac694e
ahljjun <liu.ljjun@gmail.com>
2017-07-26 01:08:32 +0800

    add swapInPairs solution

--- algorithm/SwapNodesInPairs/SwapNodesInPairs.cpp
+++ algorithm/SwapNodesInPairs/SwapNodesInPairs.cpp
@@ -18,6 +18,97 @@ Your algorithm should use only constant space. You may not modify the values in 
  */
 
 
+#include <iostream>
+using namespace std;
+
+ struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+ };
+ 
+
+
+#include <iostream>
+using namespace std;
+
+/*
 ListNode* swapPairs(ListNode* head) 
 {
+    if ( head==NULL )
+        return head;
+    if ( head->next == NULL )
+        return head;
+
+    ListNode *p = head;
+    ListNode *retListDummyHead =new ListNode(0);
+    ListNode *pRetNode = retListDummyHead;
+    while(p && p->next){
+        pRetNode->next = p->next;
+        pRetNode->next->next = p;
+        pRetNode = p;
+        p = p->next->next;
+    }
+
+    if (p)
+        pRetNode->next =  p;
+
+    return retListDummyHead->next;
 }
+*/
+
+
+
+ListNode* swapPairs(ListNode* head) 
+{
+    if ( head==NULL )
+        return head;
+    if ( head->next == NULL )
+        return head;
+
+    ListNode *p = head;
+    ListNode *retListDummyHead =new ListNode(0);
+    ListNode *pRetNode = retListDummyHead;
+    while(p && p->next){
+        ListNode* pNext = p->next;
+        ListNode* pNextNext = p->next->next;
+        pRetNode->next = pNext;
+        p->next = pNextNext;
+        pRetNode->next->next = p;
+        pRetNode = p;
+        p = pNextNext;
+    }
+
+    if (p)
+        pRetNode->next =  p;
+
+    return retListDummyHead->next;
+}
+
+ListNode* createList(int n){
+    ListNode *dummyHead = new ListNode(0);
+    ListNode *p = dummyHead;
+    for(int i=1; i<=n; i++){
+        p->next = new ListNode(i);
+        p = p->next;
+    }
+    return dummyHead->next;
+}
+
+void dumpList(ListNode *pList)
+{
+    for(ListNode *p=pList; p!=NULL; p=p->next)
+        cout<<p->val<<"->";
+
+    cout<<endl;
+}
+
+int main()
+{
+    ListNode *ll = createList(1);
+    dumpList(ll);
+    ListNode *retLL = swapPairs(ll);
+    dumpList(retLL);
+}
+
+
