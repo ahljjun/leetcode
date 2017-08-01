e448e63ca0a1ae00122814a20a7cdf0f2733b741
ahljjun <liu.ljjun@gmail.com>
2017-08-01 23:41:32 +0800

    add strStr.cpp

---
+++ algorithm/strStr/strStr.cpp
@@ -0,0 +1,10 @@
+
+/*
+Implement strStr().
+
+Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+*/
+
+
+int strStr(string haystack, string needle) {
+}
