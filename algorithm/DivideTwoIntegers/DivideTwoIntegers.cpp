f1ed3bdab334f8807009d50e1ce9155325f6283d
ahljjun <liu.ljjun@gmail.com>
2017-08-08 00:02:27 +0800

    add time-limit-exceed solution

--- algorithm/DivideTwoIntegers/DivideTwoIntegers.cpp
+++ algorithm/DivideTwoIntegers/DivideTwoIntegers.cpp
@@ -5,15 +5,41 @@ If it is overflow, return MAX_INT.
 */
 
 
-//Wrong Answer
+//Time-Limit exceed version
 int divide(int dividend, int divisor) {
-    int cnt = 0;
-    
-    // note can not use the multiplication, how to determine negative? postive?
-    while( dividend >=  divisor){
-        dividend = dividend - divisor;
-        cnt++;
-    }
-     
-    return cnt;
+        int cnt = 0;
+        bool isneg = false;
+        bool overflow = false;
+        long long ldividend = dividend;
+        long long ldivisor = divisor;
+        if ( dividend < 0 ){
+            isneg = true;
+            //dividend = -dividend; // this could overflow
+            ldividend = - dividend;
+            if ( ldividend != -dividend){
+                overflow = true;
+            }
+        }
+            
+        
+        if ( divisor < 0 ){
+            isneg = !isneg;
+            //divisor = -divisor; // this could overflow
+            ldivisor = -divisor;
+            if ( Ldivisor != -divisor){
+                overflow = true;
+            }
+        }
+        
+        if ( overflow )
+            return INT_MAX;
+        
+        while( Ldividend >=  Ldivisor){
+            dividend = dividend - divisor;
+            cnt++;
+        }
+        
+        return isNeg ? -cnt:cnt;
+  }
+
 }
